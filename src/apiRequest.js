7d3a1c85ee527c2bc50daba4236920b50ee09776
Stricks1 <gmalheiross@gmail.com>
2020-08-14 20:12:58 -0300

    refactored

---
+++ src/apiRequest.js
@@ -0,0 +1,15 @@
+async function apiRequest(place, tempType) {
+  const apiKey = '8c00744737828145f3aa9a756bb85f06';
+  try {
+    const temperature = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apiKey}&units=${tempType}`,
+      { mode: 'cors' },
+    );
+    const temp = temperature.json();
+    return Promise.resolve(temp);
+  } catch (error) {
+    return Promise.reject(alert);
+  }
+}
+
+export default apiRequest;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
-
-
-async function apiRequest(place, tempType) {
-  const apiKey = '8c00744737828145f3aa9a756bb85f06';
-  try {
-    const temperature = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apiKey}&units=${tempType}`,
-      { mode: 'cors' },
-    );
-    const temp = await temperature.json();
-    return Promise.resolve(temp);
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
+import apiRequest from './apiRequest';
 
 function component() {
   const element = document.createElement('div');
-  apiRequest('London,uk', 'metric').then((response, reject) => {
+  apiRequest('London,uk', 'metric').then((response) => {
     const returners = response;
     console.log(returners);
     element.innerHTML = returners.main.temp;
-    if (!returners.main.temp) {
-      element.innerHTML = reject;
-    }
+  }).catch(e => {
+    console.log('here')
+    e('a3');
+    console.log('here2')
   });
-
   return element;
 }
 
